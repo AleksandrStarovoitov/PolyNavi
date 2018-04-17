b092246feace1c4f5ce82ae566f83b4ce902f676
Sasha <sashok05041998@mail.ru>
2018-04-17 18:44:35 +0300

    Add project files.

---
+++ PolyNavi/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace PolyNavi
+{
+	[Activity(Label = "PolyNavi", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+		}
+	}
+}
+
