386e37014791c39fb98a32ae6101224bf02df5fe
Кирилл Большаков <kb.main@yandex.ru>
2018-04-23 19:59:51 +0300

    Added structure of project

---
+++ PolyNaviLib/BL/PolyManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolyNaviLib.BL
+{
+    public class PolyManager
+    {
+    }
+}
---
+++ PolyNaviLib/DL/SQLiteDatabase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolyNaviLib.DL
+{
+    public class SQLiteDatabase
+    {
+    }
+}
