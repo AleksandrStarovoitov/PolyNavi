a947682f71cb0c0bc5d53590a24209150a9e71c7
Sasha <starovoytov.it@mail.ru>
2018-04-24 12:36:12 +0300

    Add Repository implementation

--- PolyNaviLib/DAL/Repository.cs
+++ PolyNaviLib/DAL/Repository.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using HtmlAgilityPack;
+using PolyNaviLib.BL;
+using PolyNaviLib.SL;
+
 
 namespace PolyNaviLib.DAL
 {
 	public class Repository
 	{
+		public static Schedule GetSchedule()
+		{
+			if (false) //Есть в БД
+			{
+
+				return new Schedule();
+			}
+			else //Нет в БД
+			{
+				HtmlDocument htmlDoc = HtmlLoader.LoadHtmlDocument("addr"); //Где брать адрес?
+				Schedule schedule = ScheduleBuilder.BuildSchedule(htmlDoc);
+
+				return schedule;
+			}
+		}
+
 	}
 }
