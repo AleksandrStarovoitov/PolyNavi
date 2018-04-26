0cbbdf0a8275f7da275b6c9f5d188c1707f983f8
Кирилл Большаков <kb.main@yandex.ru>
2018-04-26 22:37:04 +0300

    Add SQLiteDatabase class methods

--- PolyNaviLib/DL/SQLiteDatabase.cs
+++ PolyNaviLib/DL/SQLiteDatabase.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+
+using SQLite;
+
+using PolyNaviLib.BL;
 
 namespace PolyNaviLib.DL
 {
 	public class SQLiteDatabase
 	{
+		SQLiteAsyncConnection db;
+
+		public SQLiteDatabase(string dbPath)
+		{
+			db = new SQLiteAsyncConnection(dbPath);
+		}
+		
+		public async Task CreateTableAsync<T>() where T : IBuisnessEntity, new()
+		{
+			await db.CreateTableAsync<T>();
+		}
+
+		public async Task<List<T>> GetItemsAsync<T>() where T : IBuisnessEntity, new()
+		{
+			return await db.Table<T>().ToListAsync();
+		}
+
+		public async Task<T> GetItem<T>(int id) where T : IBuisnessEntity, new()
+		{
+			return await db.GetAsync<T>(id);
+		}
+
+		public async Task<int> SaveItem<T>(T item) where T : IBuisnessEntity, new()
+		{
+			if (item.ID == 0)
+			{
+				return await db.InsertAsync(item);
+			}
+			else
+			{
+				return await db.UpdateAsync(item);
+			}
+		}
+
+		public async Task<int> DeleteItemAsync<T>(T item) where T : IBuisnessEntity, new()
+		{
+			return await db.DeleteAsync(item);
+		}
 	}
-
-
 }
