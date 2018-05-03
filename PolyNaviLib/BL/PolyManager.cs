3958c95613cd5540c85b6a3446c8bb1ab380f9b4
Кирилл Большаков <kb.main@yandex.ru>
2018-05-03 16:56:32 +0300

    fix merge commit

--- PolyNaviLib/BL/PolyManager.cs
+++ PolyNaviLib/BL/PolyManager.cs
@@ -6,29 +6,28 @@ using PolyNaviLib.DAL;
 
 namespace PolyNaviLib.BL
 {
+	public enum Weeks
+	{
+		Current, //Текущая неделя
+		Next     //Следующая неделя
+	}
+
+	public enum Days
+	{
+		Monday,
+		Tuesday,
+		Wednesday,
+		Thursday,
+		Friday,
+		Saturday,
+		Sunday     //,
+				   //Current, //Может понадобится
+				   //Next	   //Может понадобится
+	}
+
 	public class PolyManager
 	{
-
 		Repository repository;
-		
-		public enum Weeks
-		{
-			Current, //Текущая неделя
-			Next     //Следующая неделя
-		}
-
-		public enum Days
-		{
-			Monday,
-			Tuesday,
-			Wednesday,
-			Thursday,
-			Friday,
-			Saturday,
-			Sunday	   //,
-			//Current, //Может понадобится
-			//Next	   //Может понадобится
-		}
 
 		public PolyManager()
 		{
