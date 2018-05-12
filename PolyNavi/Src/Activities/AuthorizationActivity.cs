89bf8d7cff7df7fe88d5ae20297ef6ad287a30ca
Sasha <starovoytov.it@mail.ru>
2018-05-12 18:08:02 +0300

    Added login activity; moved big drawables to xxhdpi folder. Deleted old floor plan

---
+++ PolyNavi/Src/Activities/AuthorizationActivity.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace PolyNavi
+{
+	[Activity(Label = "PolyNavi")]
+	public class AuthorizationActivity : AppCompatActivity
+	{
+		EditText editTextAuth;
+		ISharedPreferencesEditor prefEditor;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			SetTheme(Resource.Style.MyAppTheme);
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.activity_authorization);
+
+			editTextAuth = FindViewById<EditText>(Resource.Id.edittext_auth);
+			var buttonAuth = FindViewById<Button>(Resource.Id.button_auth);
+			var textViewLater = FindViewById<TextView>(Resource.Id.textview_later_auth);
+
+			buttonAuth.Click += ButtonAuth_Click;
+			textViewLater.Click += TextViewLater_Click;
+
+			prefEditor = MainApp.Instance.SharedPreferences.Edit();
+			prefEditor.PutBoolean("auth", true).Apply();
+		}
+
+		private void ButtonAuth_Click(object sender, EventArgs e)
+		{
+			if (!editTextAuth.Text.Equals(""))
+			{
+				prefEditor.PutString("groupnumber", editTextAuth.Text).Apply();
+				ProceedToMainActivity();
+			}
+		}
+
+		private void TextViewLater_Click(object sender, EventArgs e)
+		{
+			ProceedToMainActivity();
+		}
+
+		public void ProceedToMainActivity()
+		{
+			var mainIntent = new Intent(this, typeof(MainActivity));
+			mainIntent.SetFlags(ActivityFlags.ClearTop);
+			StartActivity(mainIntent);
+			Finish();
+		}
+	}
+}
---
+++ PolyNavi/Src/Activities/MainEmptyActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PolyNavi
+{
+	[Activity(Label = "PolyNavi", MainLauncher = true)]
+	public class MainEmptyActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Create your application here
+			Intent intent;
+			if (MainApp.Instance.SharedPreferences.GetBoolean("auth", false))
+			{
+				intent = new Intent(this, typeof(MainActivity));
+			}
+			else
+			{
+				intent = new Intent(this, typeof(AuthorizationActivity));
+			}
+			StartActivity(intent);
+		}
+	}
+}
