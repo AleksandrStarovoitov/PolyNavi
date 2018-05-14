00bc2d86971fb925733594a80b2d57a3e22b4633
Кирилл Большаков <kb.main@yandex.ru>
2018-05-14 12:38:55 +0300

    Refactor Graph project

---
+++ Graph/Exceptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graph
+{
+	[Serializable]
+	public class GraphRoutingException : Exception
+	{
+		public GraphRoutingException() { }
+		public GraphRoutingException(string message) : base(message) { }
+		public GraphRoutingException(string message, Exception inner) : base(message, inner) { }
+		protected GraphRoutingException(
+		  System.Runtime.Serialization.SerializationInfo info,
+		  System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+	}
+}
