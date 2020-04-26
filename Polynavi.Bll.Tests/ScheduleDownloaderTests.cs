a3e6c5415dd34fa116ae455ab85ebc3943b07f5a
Aleksandr Starovoitov <sashok05041998@mail.ru>
2020-04-26 20:14:32 +0300

    Add schedule downloader tests.

---
+++ Polynavi.Bll.Tests/ScheduleDownloaderTests.cs
@@ -0,0 +1,65 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Moq;
+using Newtonsoft.Json;
+using Polynavi.Bll.Services;
+using Polynavi.Common.Exceptions;
+using Polynavi.Common.Models;
+using Polynavi.Common.Services;
+using Polynavi.Common.Settings;
+using Polynavi.Tests.Common;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Polynavi.Bll.Tests
+{
+    public class ScheduleDownloaderTests
+    {
+        private readonly Fixture fixture;
+
+        public ScheduleDownloaderTests()
+        {
+            fixture = FixtureInitializer.InitializeFixture();
+        }
+
+        [Fact]
+        public async Task GetScheduleFromWebAsync_Throws_NetworkException_If_No_Connection()
+        {
+            var networkChecker = new Mock<INetworkChecker>();
+            var scheduleSettings = new Mock<IScheduleSettings>();
+            var httpClientService = new Mock<IHttpClientService>();
+            networkChecker.Setup(nc => nc.IsConnected()).Returns(false);
+
+            var sut = new ScheduleDownloader(networkChecker.Object, scheduleSettings.Object,
+                httpClientService.Object);
+            Func<Task> act = async () => { await sut.GetScheduleFromWebAsync(DateTime.Now); };
+
+            await act.Should().ThrowAsync<NetworkException>();
+        }
+
+        [Fact]
+        public async Task Gets_Schedule_From_Web()
+        {
+            var date = DateTime.Now;
+            var schedule = fixture.Create<WeekSchedule>();
+            var scheduleJson = JsonConvert.SerializeObject(schedule);
+            var networkChecker = new Mock<INetworkChecker>();
+            var scheduleSettings = new Mock<IScheduleSettings>();
+            var httpClientService = new Mock<IHttpClientService>();
+            networkChecker.Setup(nc => nc.IsConnected())
+                .Returns(true);
+            httpClientService.Setup(hs => hs.GetResponseAsStringAsync(It.IsAny<string>(),
+                It.IsAny<CancellationToken>()))
+                    .ReturnsAsync(scheduleJson);
+            
+            var sut = new ScheduleDownloader(networkChecker.Object, scheduleSettings.Object,
+                httpClientService.Object);
+            var result = await sut.GetScheduleFromWebAsync(date);
+
+            result.Should().BeEquivalentTo(schedule, opt => 
+                opt.Excluding(s => s.LastUpdated));
+        }
+    }
+}
