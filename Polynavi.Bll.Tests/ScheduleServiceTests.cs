1144f70857d41edacdeb4fbbcd8a028c4a0b2b83
Aleksandr Starovoitov <sashok05041998@mail.ru>
2020-04-26 19:49:19 +0300

    Add schedule service tests. Add fixture initializer.

--- Polynavi.Bll.Tests/ScheduleServiceTests.cs
+++ Polynavi.Bll.Tests/ScheduleServiceTests.cs
@@ -1,13 +1,81 @@
-﻿using Xunit;
+﻿using AutoFixture;
+using FluentAssertions;
+using Moq;
+using Polynavi.Bll.Services;
+using Polynavi.Common.Models;
+using Polynavi.Common.Repositories;
+using Polynavi.Common.Services;
+using Polynavi.Tests.Common;
+using System;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace Polynavi.Bll.Tests
 {
     public class ScheduleServiceTests
     {
-        [Fact]
-        public void Test()
+        private readonly Fixture fixture;
+
+        public ScheduleServiceTests()
         {
-            Assert.True(true);
+            fixture = FixtureInitializer.InitializeFixture();
+        }
+
+        [Fact]
+        public async Task Gets_Latest_Schedule()
+        {
+            var date = DateTime.Now;
+            var schedule = fixture.Create<WeekSchedule>();
+            var scheduleRepository = new Mock<IScheduleRepository>();
+            var scheduleDownloader = new Mock<IScheduleDownloader>();
+            scheduleDownloader.Setup(sd => sd.GetScheduleFromWebAsync(date))
+                .ReturnsAsync(schedule);
+
+            var sut = new ScheduleService(scheduleRepository.Object, scheduleDownloader.Object);
+            var result = await sut.GetLatestAsync(date);
+
+            scheduleRepository.Verify(sr => sr.DeleteWeekAsync(date), Times.Once);
+            scheduleRepository.Verify(sr => sr.SaveScheduleAsync(schedule), Times.Once);
+            result.Should().Be(schedule);
+        }
+
+        [Fact]
+        public async Task Gets_Saved_Schedule()
+        {
+            var date = DateTime.Now;
+            var schedule = fixture.Create<WeekSchedule>();
+            var scheduleRepository = new Mock<IScheduleRepository>();
+            var scheduleDownloader = new Mock<IScheduleDownloader>();
+            scheduleRepository.Setup(sr => sr.GetScheduleAsync(date))
+                .ReturnsAsync(schedule);
+
+            var sut = new ScheduleService(scheduleRepository.Object, scheduleDownloader.Object);
+            var result = await sut.GetSavedOrLatestAsync(date);
+
+            scheduleRepository.Verify(sr => sr.GetScheduleAsync(date), Times.Once);
+            scheduleDownloader.Verify(sd => sd.GetScheduleFromWebAsync(date), Times.Never);
+            result.Should().Be(schedule);
+        }
+
+        [Fact]
+        public async Task Gets_Schedule_From_Web_If_No_Saved()
+        {
+            var date = DateTime.Now;
+            var schedule = fixture.Create<WeekSchedule>();
+            var scheduleRepository = new Mock<IScheduleRepository>();
+            var scheduleDownloader = new Mock<IScheduleDownloader>();
+            scheduleDownloader.Setup(sd => sd.GetScheduleFromWebAsync(date))
+                .ReturnsAsync(schedule);
+            scheduleRepository.Setup(sr => sr.GetScheduleAsync(date))
+                .ReturnsAsync((WeekSchedule)null);
+
+            var sut = new ScheduleService(scheduleRepository.Object, scheduleDownloader.Object);
+            var result = await sut.GetSavedOrLatestAsync(date);
+
+            scheduleRepository.Verify(sr => sr.GetScheduleAsync(date), Times.Once);
+            scheduleDownloader.Verify(sd => sd.GetScheduleFromWebAsync(date), Times.Once);
+            scheduleRepository.Verify(sr => sr.SaveScheduleAsync(schedule), Times.Once);
+            result.Should().Be(schedule);
         }
     }
 }
---
+++ Polynavi.Tests.Common/FixtureInitializer.cs
@@ -0,0 +1,19 @@
+﻿using AutoFixture;
+using System.Linq;
+
+namespace Polynavi.Tests.Common
+{
+    public static class FixtureInitializer
+    {
+        public static Fixture InitializeFixture()
+        {
+            var fixture = new Fixture();
+
+            fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+                .ForEach(b => fixture.Behaviors.Remove(b));
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+            return fixture;
+        }
+    }
+}
