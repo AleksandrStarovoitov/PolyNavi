9866c3dea4b402587dce665e69b81b8869f32353
Aleksandr Starovoitov <sashok05041998@mail.ru>
2020-05-09 18:01:41 +0300

    Add test. Add *.apk to .gitignore.

--- Polynavi.UITests/Tests.cs
+++ Polynavi.UITests/Tests.cs
@@ -1,7 +1,24 @@
-﻿namespace Polynavi.UITests
+﻿using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace Polynavi.UITests
 {
+    [TestFixture]
     public class Tests
     {
+        private const string apkPath = "com.starovoitov.polynavi.apk";
+        private IApp app;
         
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = ConfigureApp.Android.ApkFile(apkPath).StartApp();
+        }
+
+        [Test]
+        public void Test()
+        {
+            app.Repl();
+        }
     }
 }
