d096eb20b0d12b5a0213285cdae4331e5a84b1f4
Aleksandr Starovoitov <sashok05041998@mail.ru>
2020-05-10 15:21:26 +0300

    Add classes for page object pattern.

---
+++ Polynavi.UITests/BasePage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace Polynavi.UITests
+{
+    public abstract class BasePage
+    {
+        protected IApp app => AppManager.App;
+        protected bool OnAndroid => AppManager.Platform == Platform.Android;
+
+        protected abstract PlatformQuery Trait { get; }
+
+        protected BasePage()
+        {
+            AssertOnPage(TimeSpan.FromSeconds(30));
+            app.Screenshot("On " + this.GetType().Name);
+        }
+
+        /// <summary>
+        /// Verifies that the trait is still present. Defaults to no wait.
+        /// </summary>
+        /// <param name="timeout">Time to wait before the assertion fails</param>
+        protected void AssertOnPage(TimeSpan? timeout = default)
+        {
+            var message = "Unable to verify on page: " + this.GetType().Name;
+
+            if (timeout == null)
+            {
+                Assert.IsNotEmpty(app.Query(Trait.Current), message);
+            }
+            else
+            {
+                Assert.DoesNotThrow(() => app.WaitForElement(Trait.Current, timeout: timeout), message);
+            }
+        }
+
+        /// <summary>
+        /// Verifies that the trait is no longer present. Defaults to a 5 second wait.
+        /// </summary>
+        /// <param name="timeout">Time to wait before the assertion fails</param>
+        protected void WaitForPageToLeave(TimeSpan? timeout = default)
+        {
+            timeout ??= TimeSpan.FromSeconds(5);
+            var message = "Unable to verify *not* on page: " + this.GetType().Name;
+
+            Assert.DoesNotThrow(() => app.WaitForNoElement(Trait.Current, timeout: timeout), message);
+        }
+    }
+}
